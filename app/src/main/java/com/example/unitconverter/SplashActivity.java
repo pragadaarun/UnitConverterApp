ad3d58571064f8a18312f8c08def5f127c7422ea
pragadaarun <pmaniarunkumar@gmail.com>
2021-04-14 17:52:20 +0530

    Initial commit of Whole Project

---
+++ app/src/main/java/com/example/unitconverter/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
